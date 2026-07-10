4666094b4493d10765473a8da743c41ece65eb4b
stuarthardstaff29-ops <stuarthardstaff29@gmail.com>
2026-07-10 08:02:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='finplan-v1-5-0';
+const CACHE='finplan-v1-6-0';
 const ASSETS=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
